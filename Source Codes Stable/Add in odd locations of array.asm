c6515c504376d43e092cb22c4a7d1ff7d9d28deb
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-01-12 14:24:42 +0530

    Create Add in odd locations of array.asm

---
+++ Source Codes Stable/Add in odd locations of array.asm
@@ -0,0 +1,31 @@
+.MODEL small
+
+.STACK
+
+.DATA
+
+    array db 1, 2, 3, 4, 5
+    N db 2 
+ 
+.CODE
+
+.STARTUP
+
+mov cx, 00
+mov cl, N
+mov al, array[si]
+
+check:	cmp N, 2
+		ja next
+	   .EXIT
+        next:
+			mov al, array[si]
+			add al, 10
+			mov array[si], al
+			inc si
+			inc si
+			loop check
+
+.EXIT
+
+end
